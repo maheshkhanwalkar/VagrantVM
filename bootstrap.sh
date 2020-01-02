5b56ed5e1f95270c319ec3b28e509f057639e4ce
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2020-01-02 17:16:26 -0500

    setup: install jdk

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,4 @@
 apt-get update
 apt install -y build-essential cmake clang lld   # C/C++ tools
 apt install -y qemu xorriso bochs                # OS tools
+apt install -y openjdk-11-jdk                    # JDK tools
